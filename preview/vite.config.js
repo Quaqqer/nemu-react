bb24f5aef3f04b93c585869658d857d6cf391ce8
Emanuel Särnhammar <emanuel@quaqqer.com>
2026-07-16 13:54:04 +0200

    fix: remove compiled files

--- preview/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import wasm from "vite-plugin-wasm";
-export default defineConfig({
-    // @ts-expect-error
-    plugins: [wasm()],
-    server: { fs: { allow: [".", "../src", "../node_modules/"] } },
-});
